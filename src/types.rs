8ca38e0ef68deb4f1be18efed0a6804630543047
Antonio Gurgel <agurgel@thousandeyes.com>
2024-05-01 16:32:21 -0700

    Implement From<Event> for KubernetesEvent (#12)

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
 use k8s_openapi::api::core::v1::Event;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::Time;
+use k8s_openapi::chrono::Utc;
 use k8s_openapi::serde::Serialize;
 use tokio::sync::mpsc::error::SendError;
 
@@ -7,6 +8,49 @@ use tokio::sync::mpsc::error::SendError;
 pub(crate) struct KubernetesEvent {
     pub(crate) time: Time,
     pub(crate) kubernetes_event: Event,
+}
+
+impl KubernetesEvent {
+    pub(crate) fn key(&self) -> String {
+        format!(
+            "{}:{}",
+            self.kubernetes_event
+                .metadata
+                .uid
+                .as_ref()
+                .unwrap_or(&String::default()),
+            self.kubernetes_event
+                .metadata
+                .resource_version
+                .as_ref()
+                .unwrap_or(&String::default())
+        )
+    }
+}
+
+impl From<Event> for KubernetesEvent {
+    fn from(ev: Event) -> Self {
+        // Timestamp events like Fluent Bit does:
+        // https://docs.fluentbit.io/manual/pipeline/inputs/kubernetes-events#event-timestamp
+        let time = ev.last_timestamp.as_ref().cloned().unwrap_or(
+            ev.first_timestamp.as_ref().cloned().unwrap_or(
+                ev.metadata
+                    .creation_timestamp
+                    .as_ref()
+                    .cloned()
+                    .unwrap_or_else(|| {
+                        eprintln!("No timestamp in event? Using current time.");
+
+                        Time(Utc::now())
+                    }),
+            ),
+        );
+
+        Self {
+            time,
+            kubernetes_event: ev,
+        }
+    }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -23,22 +67,4 @@ pub(crate) enum KesError {
     SystemTime(#[from] std::time::SystemTimeError),
     #[error(transparent)]
     Io(#[from] std::io::Error),
-}
-
-/// Timestamp events [like Fluent Bit does](https://docs.fluentbit.io/manual/pipeline/inputs/kubernetes-events#event-timestamp):
-/// > Event timestamp will be created from the first existing field
-/// > in the following order of precendence:
-/// > lastTimestamp, firstTimestamp, metadata.creationTimestamp
-pub(crate) fn timestamp(ev: &Event) -> Time {
-    [
-        &ev.last_timestamp,
-        &ev.first_timestamp,
-        &ev.metadata.creation_timestamp,
-    ]
-    .into_iter()
-    .find_map(|opt| opt.clone())
-    // For now, assume _all_ Events have at least one usable timestamp.
-    // An Event that doesn't would act as a poison pill and repeatedly
-    // crash the program, but the possibility is incredibly remote.
-    .expect("Somehow, this Event has all three time fields unset")
 }
